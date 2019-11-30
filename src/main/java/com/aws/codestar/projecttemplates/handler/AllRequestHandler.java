f811a5435fd27b454d2e9f39da494156558c5cbf
git <fabio.casini@accenture.com>
2019-11-30 17:19:36 +0100

    delete test

--- src/main/java/com/aws/codestar/projecttemplates/handler/AllRequestHandler.java
+++ src/main/java/com/aws/codestar/projecttemplates/handler/AllRequestHandler.java
@@ -27,24 +27,6 @@ public class AllRequestHandler implements RequestHandler<Object, Object> {
         headers.put("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
         headers.put("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
 
-        Exception ex = null;
-        try {
-
-            MongoConfig config = new MongoConfig();
-            config.mongoClient();
-
-            DeliveryController deliveryController = new DeliveryController();
-            Delivery delivery = new Delivery();
-            delivery.setId("3");
-            deliveryController.create(delivery);  // test
-        }catch(Exception e){
-            ex = e;
-        }
-
-        if ( ex != null ){
-            return  new GatewayResponse(new JSONObject().put("Output", ex.getStackTrace()).toString(), headers, 500);
-        }
-
         return new GatewayResponse(new JSONObject().put("Output", "Blink Service is running").toString(), headers, 200);
     }
 }
