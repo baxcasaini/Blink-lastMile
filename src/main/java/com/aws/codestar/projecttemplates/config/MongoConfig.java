bc141309cfa53258e1d434be6ba7f916fc179e53
git <fabio.casini@accenture.com>
2019-11-30 14:45:05 +0100

    deleted dynamo

--- src/main/java/com/aws/codestar/projecttemplates/config/MongoConfig.java
+++ src/main/java/com/aws/codestar/projecttemplates/config/MongoConfig.java
@@ -2,6 +2,7 @@ package com.aws.codestar.projecttemplates.config;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import lombok.extern.slf4j.Slf4j;
@@ -13,30 +14,21 @@ import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 @Configuration
 public class MongoConfig extends AbstractMongoConfiguration {
 
-	@Value(value = "${spring.data.mongodb.database}")
-	private String db;
-    @Value(value = "${spring.data.mongodb.username}")
-    private String username;
-    @Value(value = "${spring.data.mongodb.password}")
-    private String password;
-	@Value(value = "${spring.data.mongodb.host}")
-	private String host;
-	@Value(value = "${spring.data.mongodb.port}")
-	private String port;
-	@Value(value = "${spring.data.mongodb.uri}")
-	private String uriString;
 
 	@Override
 	public MongoClient mongoClient() {
         try {
-            String encoded_pwd = URLEncoder.encode(password, "UTF-8");
+			MongoClientURI uri = new MongoClientURI(
+					"mongodb://blink-aws:blink-aws@blinkcluster-shard-00-00-pmefj.mongodb.net:27017,blinkcluster-shard-00-01-pmefj.mongodb.net:27017,blinkcluster-shard-00-02-pmefj.mongodb.net:27017/test?ssl=true&replicaSet=BlinkCluster-shard-0&authSource=admin&retryWrites=true&w=majority");
 
-            String client_url = "mongodb://blink-aws:blink-aws@blinkcluster-shard-00-00-pmefj.mongodb.net:27017,blinkcluster-shard-00-01-pmefj.mongodb.net:27017,blinkcluster-shard-00-02-pmefj.mongodb.net:27017/test?ssl=true&replicaSet=BlinkCluster-shard-0&authSource=admin&retryWrites=true&w=majority";
-            MongoClientURI uri = new MongoClientURI(client_url);
+			MongoClient mongoClient = new MongoClient(uri);
+			MongoDatabase database = mongoClient.getDatabase("test");
 
-            return new MongoClient(uri);
+			MongoClient mc = new MongoClient(uri);
 
-        } catch (UnsupportedEncodingException ex) {
+            return mc;
+
+        } catch (Exception ex) {
             log.error(ex.getMessage());
         }
         return null;
@@ -44,6 +36,6 @@ public class MongoConfig extends AbstractMongoConfiguration {
 
 	@Override
 	protected String getDatabaseName() {
-		return db;
+		return "BlinkCluster";
 	}
 }
--- src/main/java/com/aws/codestar/projecttemplates/controller/DeliveryController.java
+++ src/main/java/com/aws/codestar/projecttemplates/controller/DeliveryController.java
@@ -2,50 +2,38 @@ package com.aws.codestar.projecttemplates.controller;
 
 import com.aws.codestar.projecttemplates.DTO.DeliveryDTO;
 import com.aws.codestar.projecttemplates.model.Delivery;
-import com.aws.codestar.projecttemplates.repository.DeliveryRepository;
 import java.util.ArrayList;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/deliveries")
 public class DeliveryController {
 
-    @Autowired
-    private DeliveryRepository deliveryRepository;
-
     @GetMapping
     public List<Delivery> getAll() {
-        return getListDeliveryFromDTO(deliveryRepository.findAll());
+        return null;
     }
 
     @GetMapping("/{id}")
     public Delivery getById(@PathVariable("id") String id) {
-        return getDeliveryFromDTO(deliveryRepository.findById(id).get());
+        return null;
     }
 
     @PostMapping
     public Delivery create(@RequestBody Delivery delivery) {
-        return getDeliveryFromDTO(deliveryRepository.insert(getDTOfromDelivery(delivery)));
+        return null;
     }
 
     @DeleteMapping("/{id}")
     public void delete(@PathVariable("id") String id) {
-        deliveryRepository.deleteById(id);
+        return;
     }
 
     @PatchMapping("/{id}")
-    public Delivery update(@PathVariable("id") String id, @RequestBody Delivery delivery) {
-        DeliveryDTO deliveryUp = deliveryRepository.findById(id).get();
-        if(delivery.getCustomer() != null)
-        	deliveryUp.setCustomer(delivery.getCustomer());
-        if(delivery.getReceiver() != null)
-        	deliveryUp.setReceiver(delivery.getReceiver());
-        if(delivery.getPackages() != null)
-        	deliveryUp.setPackages(delivery.getPackages());
+    public Object update(@PathVariable("id") String id, @RequestBody Delivery delivery) {
         
-    	return getDeliveryFromDTO(deliveryRepository.save(deliveryUp));
+    	return null;
     }
 
     private Delivery getDeliveryFromDTO(DeliveryDTO dto) {
